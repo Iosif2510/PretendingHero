b33fb15f62b6defc33749a837c428dabb715d45c
Iosif2510 <gpotatok@gmail.com>
2023-01-06 20:22:16 +0900

    Singleton Pattern Added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:70493e752665b6d46859286ee57baeb7a29f2501bd5de72f6a0e5fe0f0308c18
+size 309
---
+++ Assets/Scripts/UnitySingleton.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:71e5eac13a7590689ab7f80dac9c83eb0e758286428fe86f44dfc988b6947558
+size 952
