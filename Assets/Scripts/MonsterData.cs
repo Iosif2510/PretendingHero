861e318e4e06e8344f1da7b88c1bc224e8a3dd27
jych <jych1109@gmail.com>
2023-01-07 02:40:42 +0900

    dataAssetsUpdate
    몬스터가 level을 올릴 방법을 마땅히 생각나지 않아서 그냥 태생적 레벨을 넣었습니다. 희귀도에서 0 : common, 1 : Rare, 2 : Epic, 3 : Unique 입니다.

--- Assets/Scripts/MonsterData.cs
+++ Assets/Scripts/MonsterData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b428a58a62fefce9867d1d31c28f0f35bc584032abd808956354077c1852c3e6
-size 298
+oid sha256:a1c4832f43b1d9edf0177c4c1014f15f9cb15c567fd76c6b1b7ae8d5723a8d17
+size 367
