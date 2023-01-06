102318a93dafa2e63d196f41fb17df75f7984f26
Minseo Jeon <minseo4565@snu.ac.kr>
2023-01-06 23:24:11 +0900

    ADD Attack, Animation, Interaction

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:816caec2f1bee05ffd969e59b111978cdce12020996935ac44abe742c2da3ecd
-size 813
+oid sha256:6e02f8ec16095133c5477f9455a7baf24e46284a6634d9a47e8e1f438c5a2935
+size 3283
