27c701909739649660089833b6d343052fd1fe7e
Iosif2510 <gpotatok@gmail.com>
2023-01-07 00:30:59 +0900

    fix

--- Assets/Scripts/MonsterData.cs
+++ Assets/Scripts/MonsterData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f3d1f5409285bbfe9005be9619fb2234e5fb7b6db70357add9e008ff57d7670
-size 300
+oid sha256:b428a58a62fefce9867d1d31c28f0f35bc584032abd808956354077c1852c3e6
+size 298
