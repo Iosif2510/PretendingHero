8e18ecb8469d33699f087beca5c869f8ab9215ba
Minseo Jeon <minseo4565@snu.ac.kr>
2023-01-07 22:17:06 +0900

    ADD TrapRemove, Rescue

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a350dd5ceed346ccce2b609cd4d5779ead0f7d34af9fc04c334a18b7fbc8d13a
-size 7533
+oid sha256:caf110997d6993248f9addb96394957522577cfb75ee5cba933f62356198aea3
+size 7639
--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f205d5015c82644a1d3294cb6a29ad45a6a9e8afb9b72bef75533160b3fabdec
-size 10982
+oid sha256:7134cef87fa38f61b105f7eb354c6a873e7102be4fff06c49f02af3a1babdd12
+size 11306
