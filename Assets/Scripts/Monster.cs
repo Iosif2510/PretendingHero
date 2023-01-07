3b7177de92859d07e7e582dc4725242852045b7b
Jaeyihello <hellojaeyi0709@gmail.com>
2023-01-07 14:10:17 +0900

    j0107

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f5c9a5fc62cd00000b11559218f691ce3a7eb87c202996b3b2131bedcc59c31
-size 1892
+oid sha256:dd9d4ad5218b95e3532ba2e5b64e523c884ddfc383db9682129033d218d816dc
+size 2382
