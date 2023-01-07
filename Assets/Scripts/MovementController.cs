46e56a884b1690783b2966457b09c3526f34195f
Minseo Jeon <minseo4565@snu.ac.kr>
2023-01-07 14:04:20 +0900

    Add guard roughly

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:277a1792cf9bf3ec2d6ee379b4b09f656b0cbbd2681e3a45d75a4061ad29c1f8
-size 8053
+oid sha256:a64e3235ba7028bc88daa553694af34b8ee2cdba756e82a30814cd52b93fea05
+size 7949
