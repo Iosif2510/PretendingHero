9e5b0861dec3680d5d2146fa9cebc7fe192a617e
Minseo Jeon <minseo4565@snu.ac.kr>
2023-01-07 17:00:41 +0900

    Monster modif 1

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d6cc39407c2bb27bbc06451b111964eaad360bcc17986d2d7ea3e19e4e8a204
-size 2384
+oid sha256:07cbf5f6ff74b74b59d9849ef0224bf9fcf260f567ada3ce39ede1de918dfd40
+size 2454
