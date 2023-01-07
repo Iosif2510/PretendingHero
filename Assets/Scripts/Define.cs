575ddc4d61d3290ac25984400b35ab5a28b3d6f9
Minseo Jeon <minseo4565@snu.ac.kr>
2023-01-07 21:29:25 +0900

    asdf

--- Assets/Scripts/Define.cs
+++ Assets/Scripts/Define.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7eaed2cfdc4b06afd52956dae9859dacea662c62ddf3f107646ebbf503f9f1fc
-size 461
+oid sha256:4d62b9f0c851805232669cf2d1317d94a3612a3e6e92bd5615d7dd85de427073
+size 462
