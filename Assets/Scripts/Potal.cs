ba824e997d0bf409e308655682610bc901eafbe7
Iosif2510 <gpotatok@gmail.com>
2023-01-07 12:42:03 +0900

    Portal Fix

--- Assets/Scripts/Potal.cs
+++ Assets/Scripts/Potal.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ecffd2698570cb236541168920a276ad6eb2fc4db3b7a37f728cba60e3aeac0e
-size 399
+oid sha256:2de1f7eb96ec7f5b4eb59112790f9a2b87794bcb1f2213677aa60a7251762a59
+size 467
