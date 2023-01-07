892a70700c1084f86a2d2409b5e14ada0876db43
Iosif2510 <gpotatok@gmail.com>
2023-01-07 20:51:31 +0900

    .

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cff2a32c4c558f12df0aeb91a2cb97b1be348f7d7110a5c252fb056eccb4f834
+oid sha256:21491249c4b6e492d9a42df1fae7c254ba06a0113e0170986489fbf649f37c8f
 size 1266
