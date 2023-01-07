69d859c19211241e1a511c3a23671bdd0e971d3e
Minseo Jeon <minseo4565@snu.ac.kr>
2023-01-07 13:41:14 +0900

    Dash, afterImage, Concentration, Transparent (Temp: HP, EXP, UI)\n

---
+++ Assets/Scripts/CooldownUI.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e323bc23603690dcf68d15622997b2c5fb48752b1138375ebf0884f3d240599d
+size 754
---
+++ Assets/Scripts/MinseoTest/TMP_UI.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c0af9d1a6cf21f7c5f28296538afe2eff975f4fd004163c207afcc41d06a896a
+size 804
